52c4733bab971c893600a0d21d555644048178b2
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2018-02-12 16:36:55 -0600

    feat: Adding angular material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// importar animaciones para el navegador web
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// importar los componentes a utlizar
+import { MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Soporte para eventos de touch
+import 'hammerjs';
+
 if (environment.production) {
   enableProdMode();
 }
