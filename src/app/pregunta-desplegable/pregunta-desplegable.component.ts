15332d56c948f45d5eb0176615a12f962ee03a19
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2018-02-13 01:20:27 -0600

    feat(all): Creating functionality for desplegable questions, now the user can add respuestas

--- src/app/pregunta-desplegable/pregunta-desplegable.component.ts
+++ src/app/pregunta-desplegable/pregunta-desplegable.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-pregunta-desplegable',
   templateUrl: './pregunta-desplegable.component.html',
-  styleUrls: ['./pregunta-desplegable.component.scss']
+  styleUrls: ['./pregunta-desplegable.component.scss', '../shared-styles/respuestas.scss']
 })
 export class PreguntaDesplegableComponent implements OnInit {
   @Input() info: Object;
@@ -11,5 +11,10 @@ export class PreguntaDesplegableComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  agregarRespuesta () {
+    this.info.opciones.push({ valor: '', viewValue: ''});
+  }
+
 
 }
